52a9a7984233d69c4f109d359ac59b4341027842
iniquitous <julia.wang@live.nl>
2026-03-10 14:09:20 +0100

    added favorite pagina

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ require('dotenv').config()
 // ===============================
 const express = require('express')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
-
+const fetchFn = global.fetch
+  ? global.fetch
+  : (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args))
 // ===============================
 // Express setup
 // ===============================
@@ -42,8 +44,6 @@ client.connect()
   .then(() => {
     console.log('Database connection established');
     // Hier selecteer je de juiste database en collectie uit je .env
-    const db = client.db(process.env.DB_NAME);
-    collection = db.collection(process.env.DB_COLLECTION);
   })
   .catch((err) => {
     console.log(`Database connection error - ${err}`);
@@ -53,7 +53,8 @@ client.connect()
 // ===============================
 // Data
 // ===============================
-
+    //const db = client.db(process.env.DB_NAME);
+    //collection = db.collection(process.env.DB_COLLECTION);
 
 app.get('/kaartje', async (req, res) => {
 
@@ -64,7 +65,6 @@ app.get('/kaartje', async (req, res) => {
       
       res.render('partials/kaartje', { data: data }); 
 });
-
 
 
 // ===============================
@@ -86,6 +86,43 @@ app.get('/detail/:jobID', (req, res) => {
   //in de db  zoeken
   console.log(req.params.jobID)
   res.send("job id = " +req.params.jobID); 
+});
+
+app.get('/favourites', (req, res) => {
+  const jobs = [
+    {
+      _id: '1',
+      title: 'Medior Business Developer Warmte',
+      company: 'Alliander',
+      date: 'Wed, 04 Mar 2026 03:48:58 GMT',
+      description: 'Als Medior Business <b>Developer</b> Warmte versnel je de warmtetransitie...',
+      locations: 'Amsterdam, Noord-Holland',
+      salary: '€5310 - 9016 per month',
+      url: '#'
+    },
+    {
+      _id: '2',
+      title: 'Frontend Developer',
+      company: 'Booking.com',
+      date: 'Tue, 02 Mar 2026 03:48:58 GMT',
+      description: 'Work on modern frontend applications and scalable UI systems...',
+      locations: 'Amsterdam',
+      salary: '€4500 - 6500 per month',
+      url: '#'
+    },
+    {
+      _id: '3',
+      title: 'Node.js Backend Developer',
+      company: 'Adyen',
+      date: 'Mon, 01 Mar 2026 03:48:58 GMT',
+      description: 'Build backend services for global payment infrastructure...',
+      locations: 'Amsterdam',
+      salary: '€5200 - 7200 per month',
+      url: '#'
+    }
+  ];
+
+  res.render('pages/favorites', { jobs });
 });
 
 // ===============================
