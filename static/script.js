7e955f7a4b48b488615087544b0f12458232f3f9
JuliaPoort <julia.poort.poort@hva.nl>
2026-03-30 14:56:08 +0200

    Code van luna menu

--- static/script.js
+++ static/script.js
@@ -49,6 +49,12 @@ if (toggle) {
     }
   });
 }
+function toggleSidebar() {
+  const layout = document.getElementById("mainLayout");
+  layout.classList.toggle("sidebar-collapsed");
+}
+ 
+ 
 
 //================================
 // Melding profielfoto
